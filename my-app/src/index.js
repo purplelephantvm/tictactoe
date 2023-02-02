f2bf0434ac9a02a6fba0c608f06ecf51195cbaa8
Ian Yuen <ianhugo@amazon.com>
2023-02-02 10:06:14 -0800

    push

---
+++ my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import "./index.css";
+
+// import React from 'react';
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
